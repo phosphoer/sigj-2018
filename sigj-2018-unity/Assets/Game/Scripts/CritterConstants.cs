01165eb38bab37f4cfdf25b37d455c26b5d2e391
HipsterSloth <hipstersloth908@gmail.com>
2018-11-02 22:31:48 -0700

    Adding stub classes for critter orders

---
+++ sigj-2018-unity/Assets/Game/Scripts/CritterConstants.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CritterConstants {
+  public enum CreatureColor
+  {
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Purple
+  }
+
+  public enum CreatureShape
+  {
+    FloatingOrb,
+    SlimeBlobGumdrop,
+    CrawlingWorm,
+    FloatingPeanut,
+    LumpyCloud,
+    PulsatingStarfish
+  }
+
+  public enum CreatureSize
+  {
+    Small,
+    Medium,
+    Large
+  }
+}
