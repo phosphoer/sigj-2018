de3613ad6b3cc37752212774373260cbf2cd6b3c
Marshall <marshall_parsons@hotmail.com>
2018-11-03 13:01:31 -0700

    Animations!
    And a shitty proto system for attachments.

---
+++ sigj-2018-unity/Assets/Game/Scripts/AttchmntAnimMngr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttchmntAnimMngr : MonoBehaviour {
+
+	public int AnimCount = 3;
+	public Vector2 PostAnimDelay = new Vector2(.25f,1f);
+	public Vector2 AdditionalRandomScale = new Vector2(1f,1f);
+
+	Animator anim;
+	bool delaying = false;
+	float currentWait = 0f;
+	float timer = 0f;
+
+	void Awake(){
+		transform.localScale *= Random.Range(AdditionalRandomScale.x,AdditionalRandomScale.y);
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentWait = Random.Range(PostAnimDelay.x,PostAnimDelay.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(delaying){
+			timer+=Time.deltaTime;
+			if(timer>=currentWait){
+				delaying = false;
+				ChooseNewAnim();
+				timer = 0f;
+			}
+		}
+		else{
+			if(anim.GetCurrentAnimatorStateInfo(0).IsName("HubState")){
+				delaying = true;
+				currentWait = Random.Range(PostAnimDelay.x,PostAnimDelay.y);
+			}
+		}
+	}
+
+	public void ChooseNewAnim(){
+		anim.SetInteger("AnimBranch",Random.Range(0,AnimCount));
+	}
+}
---
+++ sigj-2018-unity/Assets/Game/Scripts/Proto/ProtoAttacher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoAttacher : MonoBehaviour {
+
+	public GameObject[] Attachments = new GameObject[0];
+	public Vector2 ScaleMinMax = new Vector2(1.2f,.8f);
+	public Vector2 RotateMinMax = new Vector2(0f,90f);
+
+
+	List<ProtoAttachPoint> points = new List<ProtoAttachPoint>();
+	List<GameObject> attachments = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+
+		PlaceNewAttachments();
+
+	}
+
+
+	public void PlaceNewAttachments(){
+
+		if(attachments.Count>0){
+			for(int i=0; i<attachments.Count; i++){
+				Destroy(attachments[i]);
+			}
+			attachments.Clear();
+		}
+
+		points.Clear();
+		points.AddRange(transform.GetComponentsInChildren<ProtoAttachPoint>(false));
+
+		for(int i=0; i<points.Count; i++){
+			int choice = Random.Range(0,Attachments.Length-1);
+			GameObject temp = Instantiate(Attachments[choice],points[i].transform) as GameObject	;
+			temp.transform.localPosition = Vector3.zero;
+			temp.transform.localEulerAngles = new Vector3(0f,0f,Random.Range(RotateMinMax.x, RotateMinMax.y));
+			temp.transform.localScale  *= Random.Range(ScaleMinMax.x,ScaleMinMax.y);
+			attachments.Add(temp);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
