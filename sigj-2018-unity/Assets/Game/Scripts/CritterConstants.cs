1b936fc5f84cb5aa0b3a7ea2c07fb72cae19c51f
HipsterSloth <hipstersloth908@gmail.com>
2018-11-02 23:17:55 -0700

    Updating critter color enum and adding enum to ui string functions

--- sigj-2018-unity/Assets/Game/Scripts/CritterConstants.cs
+++ sigj-2018-unity/Assets/Game/Scripts/CritterConstants.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public static class CritterConstants {
   public enum CreatureColor
   {
+    White,
     Red,
-    Orange,
-    Yellow,
-    Green,
     Blue,
+    Orange,
+    Green,
+    Yellow,
     Purple
+  }
+
+  public static string GetCreatureColorDisplayString(CreatureColor Color)
+  {
+    switch(Color) {
+      case CreatureColor.White:
+        return "White";
+      case CreatureColor.Red:
+        return "Red";
+      case CreatureColor.Blue:
+        return "Blue";
+      case CreatureColor.Orange:
+        return "Orange";
+      case CreatureColor.Green:
+        return "Green";
+      case CreatureColor.Yellow:
+        return "Yellow";
+      case CreatureColor.Purple:
+        return "Purple";
+    }
+
+    return "INVALID";
   }
 
   public enum CreatureShape
@@ -23,10 +46,44 @@ public static class CritterConstants {
     PulsatingStarfish
   }
 
+  public static string GetCreatureShapeDisplayString(CreatureShape Shape)
+  {
+    switch (Shape) {
+      case CreatureShape.FloatingOrb:
+        return "Floating Orb";
+      case CreatureShape.SlimeBlobGumdrop:
+        return "Slime Blob";
+      case CreatureShape.CrawlingWorm:
+        return "Crawling Worm";
+      case CreatureShape.FloatingPeanut:
+        return "Floating Peanut";
+      case CreatureShape.LumpyCloud:
+        return "Lumpy Cloud";
+      case CreatureShape.PulsatingStarfish:
+        return "Starfish";
+    }
+
+    return "INVALID";
+  }
+
   public enum CreatureSize
   {
     Small,
     Medium,
     Large
+  }
+
+  public static string GetCreatureSizeDisplayString(CreatureSize Size)
+  {
+    switch (Size) {
+      case CreatureSize.Small:
+        return "Small";
+      case CreatureSize.Medium:
+        return "Medium";
+      case CreatureSize.Large:
+        return "Large";
+    }
+
+    return "INVALID";
   }
 }
