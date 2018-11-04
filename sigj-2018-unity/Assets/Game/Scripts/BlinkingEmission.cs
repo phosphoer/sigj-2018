27df0c7463cebde1f5ba68c4ee6231670621d3e0
David Evans <david0evans@gmail.com>
2018-11-03 21:13:32 -0700

    Fix warning

--- sigj-2018-unity/Assets/Game/Scripts/BlinkingEmission.cs
+++ sigj-2018-unity/Assets/Game/Scripts/BlinkingEmission.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlinkingEmission : MonoBehaviour {
-	
-	Renderer renderer;
-    Material mat;
-	
-	public float Speed;
-	public Color baseColor;	
+public class BlinkingEmission : MonoBehaviour
+{
+  public float Speed;
+  public Color baseColor;
 
-	// Use this for initialization
-	void Start () {
-		renderer = GetComponent<Renderer> ();
-		mat = renderer.material;
-	}
-	
- void Update () {
- 
-         float emission = Mathf.PingPong (Time.time * Speed, 1.0f); 
-         Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
-         mat.SetColor ("_EmissionColor", finalColor);
-     }
+  private Renderer blinkRenderer;
+  private Material mat;
+
+  private void Start()
+  {
+    blinkRenderer = GetComponent<Renderer>();
+    mat = blinkRenderer.material;
+  }
+
+  private void OnDestroy()
+  {
+    Destroy(mat);
+  }
+
+  private void Update()
+  {
+    float emission = Mathf.PingPong(Time.time * Speed, 1.0f);
+    Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+    mat.SetColor("_EmissionColor", finalColor);
+  }
 }
