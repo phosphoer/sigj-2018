2deb58330412d65e1b91b0d1640b19502b7c8563
Jeff Sparks <jeff-sparks@hotmail.com>
2018-11-03 17:51:59 -0700

    Updates to Level, Restoring Marshall's material or whatever

---
+++ sigj-2018-unity/Assets/Game/Scripts/RotateConstantly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateConstantly : MonoBehaviour {
+
+public float xspeed = 0;
+public float yspeed = 50;
+public float zspeed = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (xspeed,yspeed,zspeed*Time.deltaTime);
+	}
+}
