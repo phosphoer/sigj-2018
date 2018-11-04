3745ee6b44d13c35aaf81995ae709e0c61e11745
HipsterSloth <hipstersloth908@gmail.com>
2018-11-03 18:55:14 -0700

    Added new attachment manager and refactored customer request system

---
+++ sigj-2018-unity/Assets/Game/Scripts/CritterAttachmentManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CreatureAttachmentDescriptor
+{
+  public string AttachmentType = "";
+  public string UISingularName = "";
+  public string UIPluralName = "";
+  public GameObject[] Variations = new GameObject[0];
+}
+
+public class CritterAttachmentManager : MonoBehaviour {
+
+  public Vector2 ScaleMinMax = new Vector2(1.2f, .8f);
+  public Vector2 RotateMinMax = new Vector2(0f, 90f);
+
+  List<ProtoAttachPoint> attachPoints = new List<ProtoAttachPoint>();
+  List<GameObject> attachments = new List<GameObject>();
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  public void SpawnAttachments(CreatureDescriptor SpawnDescriptor)
+  {
+    // Clear off any previous attachments
+    if (attachments.Count > 0) {
+      for (int i = 0; i < attachments.Count; i++) {
+        Destroy(attachments[i]);
+      }
+      attachments.Clear();
+    }
+
+    // Gather all available attach points
+    attachPoints.Clear();
+    attachPoints.AddRange(transform.GetComponentsInChildren<ProtoAttachPoint>(false));
+
+    // Shuffle the indices of the attach points
+    int[] shuffledAttachPointIndices= ArrayUtilities.MakeShuffledIntSequence(0, attachPoints.Count - 1);
+
+    // Spawn attachments
+    int attachmentCount = System.Math.Min(SpawnDescriptor.Attachments.Length, attachPoints.Count);
+    for (int attachmentIndex = 0; attachmentIndex < attachmentCount; attachmentIndex++) {
+      int attachPointIndex = shuffledAttachPointIndices[attachmentIndex];
+      ProtoAttachPoint AttachPoint = attachPoints[attachPointIndex];
+      GameObject AttachmentPrefab= SpawnDescriptor.Attachments[attachmentIndex];
+
+      GameObject attachment = Instantiate(AttachmentPrefab, AttachPoint.transform) as GameObject;
+      attachment.transform.localPosition = Vector3.zero;
+      attachment.transform.localEulerAngles = new Vector3(0f, 0f, Random.Range(RotateMinMax.x, RotateMinMax.y));
+      attachment.transform.localScale *= Random.Range(ScaleMinMax.x, ScaleMinMax.y);
+      attachments.Add(attachment);
+    }
+  }
+}
--- sigj-2018-unity/Assets/Game/Scripts/CustomerOrder.cs
+++ sigj-2018-unity/Assets/Game/Scripts/CustomerOrder.cs
@@ -3,9 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+public class CustomerDesire
+{
+  public enum DesireType { Nothing, ChangeColor, ChangeShape, ChangeSize, ChangeAttachments };
+
+  public virtual DesireType GetDesireType() {
+    return DesireType.Nothing;
+  }
+
+  public virtual string ToUIString() {
+    return "";
+  }
+}
+
+[System.Serializable]
+public class CustomerColorDesire : CustomerDesire
+{
+  public CritterConstants.CreatureColor DesiredColor = CritterConstants.CreatureColor.White;
+
+  public override DesireType GetDesireType()
+  {
+    return DesireType.ChangeColor;
+  }
+
+  public override string ToUIString()
+  {
+    return string.Format("Color: {0}", CritterConstants.GetCreatureColorDisplayString(DesiredColor));
+  }
+}
+
+[System.Serializable]
+public class CustomerShapeDesire : CustomerDesire
+{
+  public CritterConstants.CreatureShape DesiredShape = CritterConstants.CreatureShape.FloatingOrb;
+
+  public override DesireType GetDesireType()
+  {
+    return DesireType.ChangeShape;
+  }
+
+  public override string ToUIString()
+  {
+    return string.Format("Shape: {0}", CritterConstants.GetCreatureShapeDisplayString(DesiredShape));
+  }
+}
+
+[System.Serializable]
+public class CustomerSizeDesire : CustomerDesire
+{
+  public CritterConstants.CreatureSize DesiredSize = CritterConstants.CreatureSize.Large;
+
+  public override DesireType GetDesireType()
+  {
+    return DesireType.ChangeSize;
+  }
+
+  public override string ToUIString()
+  {
+    return string.Format("Size: {0}", CritterConstants.GetCreatureSizeDisplayString(DesiredSize));
+  }
+}
+
+[System.Serializable]
+public class CreatureAttachmentDesire : CustomerDesire
+{
+  public CreatureAttachmentDescriptor AttachmentType;
+  public int Count;
+
+  public override DesireType GetDesireType()
+  {
+    return DesireType.ChangeAttachments;
+  }
+
+  public override string ToUIString()
+  {
+    if (Count > 1) {
+      return string.Format("{0} {1}", Count, AttachmentType.UIPluralName);
+    }
+    else if (Count == 1) {
+      return string.Format("1 {0}", AttachmentType.UISingularName);
+    }
+    else 
+    {
+      return string.Format("No {0}", AttachmentType.UIPluralName);
+    }
+  }
+}
+
+[System.Serializable]
+public class CreatureDescriptor
+{
+  public CritterConstants.CreatureColor Color= CritterConstants.CreatureColor.White;
+  public CritterConstants.CreatureShape Shape= CritterConstants.CreatureShape.FloatingOrb;
+  public CritterConstants.CreatureSize Size= CritterConstants.CreatureSize.Small;
+  public GameObject[] Attachments = new GameObject[0];
+}
+
+[System.Serializable]
 public class CustomerOrder {
   public int OrderNumber;
-  public CritterConstants.CreatureColor DesiredColor;
-  public CritterConstants.CreatureShape DesiredShape;
-  public CritterConstants.CreatureSize DesiredSize;
+  public CreatureDescriptor SpawnDescriptor;
+  public CustomerDesire[] CustomerDesires = new CustomerDesire[0];
 }
