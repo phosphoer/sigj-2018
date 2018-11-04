53a1aa7bffa117a5d435f21c33dea42c1f86bb0f
Marshall <marshall_parsons@hotmail.com>
2018-11-03 20:34:34 -0700

    merge

---
+++ sigj-2018-unity/Assets/Game/Scripts/BlinkingEmission.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingEmission : MonoBehaviour {
+	
+	Renderer renderer;
+    Material mat;
+	
+	public float Speed;
+	public Color baseColor;	
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+		mat = renderer.material;
+	}
+	
+ void Update () {
+ 
+         float emission = Mathf.PingPong (Time.time * Speed, 1.0f); 
+         Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+         mat.SetColor ("_EmissionColor", finalColor);
+     }
+}
---
+++ sigj-2018-unity/Assets/Game/Scripts/TextureSlide.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TextureSlide : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    public float scrollSpeedx = 0;
+    public float scrollSpeedy = 0.5f;
+	
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        float offsetx = Time.time * scrollSpeedx;
+        float offsety = Time.time * scrollSpeedy;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offsetx, offsety));
+    }
+}
