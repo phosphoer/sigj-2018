8e43b54dff68bb48f7c0a57c43ed90e686b0c29a
Marshall <marshall_parsons@hotmail.com>
2018-11-04 11:52:49 -0800

    Sprucing up this sandwich a bit..

--- sigj-2018-unity/Assets/Game/Scripts/Sandwich.cs
+++ sigj-2018-unity/Assets/Game/Scripts/Sandwich.cs
@@ -7,7 +7,10 @@ public class Sandwich : MonoBehaviour {
 	public Renderer[] SandwichBits = new Renderer[0];
 	public float CrumbEmitForceThreshold = .25f;
 	public ParticleSystem CrumbsFX;
+	public float CritterEatCoolDown = 2f;
 
+	bool cding = false;
+	float timer = 0f;
 	int sandwichLvl = -1;
 	Rigidbody rb;
 
@@ -21,18 +24,39 @@ public class Sandwich : MonoBehaviour {
 		SetSandwichLevel(0);
 	}
 
-
-	public void SetSandwichLevel(int newLvl){
-		if(sandwichLvl>0) SandwichBits[sandwichLvl].enabled = false;
-
-		sandwichLvl = newLvl;
-		SandwichBits[sandwichLvl].enabled = true;
-		CrumbsFX.Play();
+	void Update(){
+		if(cding){
+			timer+=Time.deltaTime;
+			if(timer>=CritterEatCoolDown){
+				cding = false;
+				timer = 0f;
+			}
+		}
 	}
 
-	void OnCollisionEnter(){
+	public void SetSandwichLevel(int newLvl){
+		CrumbsFX.Play();
+		if(newLvl<SandwichBits.Length){
+			if(sandwichLvl>=0) SandwichBits[sandwichLvl].enabled = false;
+				
+			sandwichLvl = newLvl;
+			SandwichBits[sandwichLvl].enabled = true;
+		}
+		else Destroy(gameObject);
+	}
+
+	void OnCollisionEnter(Collision col){
 		if(rb.velocity.magnitude >= CrumbEmitForceThreshold){
 			CrumbsFX.Play();
+		}
+
+		CritterController hitCritter = col.transform.GetComponentInParent<CritterController>();
+		if(!hitCritter) Debug.LogWarning("No critter controller found on object: " + col.transform.parent.parent.name);
+
+		if(!cding && hitCritter){
+			Debug.LogWarning("Critter fed!");
+			SetSandwichLevel(sandwichLvl+1);
+			cding = true;
 		}
 	}
 }
