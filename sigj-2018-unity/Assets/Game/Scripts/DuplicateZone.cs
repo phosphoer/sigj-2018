1fb996d45419b787c1acdec7211098793541efde
David Evans <david0evans@gmail.com>
2018-11-03 21:48:52 -0700

    wait a bit longer before activating cloner

--- sigj-2018-unity/Assets/Game/Scripts/DuplicateZone.cs
+++ sigj-2018-unity/Assets/Game/Scripts/DuplicateZone.cs
@@ -58,7 +58,7 @@ public class DuplicateZone : MonoBehaviour
     if (_duplicateEffectToggle != null)
       _duplicateEffectToggle.SetActive(true);
 
-    yield return new WaitForSeconds(1.0f);
+    yield return new WaitForSeconds(3.0f);
 
     if (_duplicateEffectPrefab != null)
     {
