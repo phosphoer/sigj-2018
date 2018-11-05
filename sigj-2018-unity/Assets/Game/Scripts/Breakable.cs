e8edf7178c8c5097b354f68c8d051023e436c80d
Marshall <marshall_parsons@hotmail.com>
2018-11-04 17:02:37 -0800

    Adding breakable pot!

---
+++ sigj-2018-unity/Assets/Game/Scripts/Breakable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+	public GameObject DisableOnBreak;
+	public GameObject EnableOnBreak;
+	public Rigidbody ToDisable;
+	public float BreakingPoint = 2f;
+
+	bool BROKEN = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter(Collision col){
+		if(col.impulse.magnitude >= BreakingPoint){
+			BROKEN = true;
+			if(DisableOnBreak) DisableOnBreak.SetActive(false);
+			if(EnableOnBreak) EnableOnBreak.SetActive(true);
+			if(ToDisable) ToDisable.isKinematic = true;
+		}
+	}
+}
