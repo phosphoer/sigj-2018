3f8c4a286a4f49e2795ba4b3ca1928620c8b7b0a
David Evans <david0evans@gmail.com>
2018-11-04 18:08:38 -0800

    fix some sound stuff

--- sigj-2018-unity/Assets/Game/Scripts/RigidBodySFX.cs
+++ sigj-2018-unity/Assets/Game/Scripts/RigidBodySFX.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidBodySFX : MonoBehaviour {
+public class RigidBodySFX : MonoBehaviour
+{
+  public SoundBank SFX;
+  public float VelocityCuttoff = 1f;
+  public float SFXCoolDown = .5f;
 
-	public SoundBank SFX;
-	public float VelocityCuttoff = 1f;
-	public float SFXCoolDown = .5f;
+  float cdTimer = 0f;
+  bool cding = false;
 
-	float cdTimer = 0f;
-	bool cding = false;
 
-	
-	// Update is called once per frame
-	void Update () {
-		if(cding){
-			cdTimer+=Time.deltaTime;
-			if(cdTimer>=SFXCoolDown){
-				cdTimer = 0f;
-				cding = false;
-			}
-		}
-	}
+  // Update is called once per frame
+  void Update()
+  {
+    if (cding)
+    {
+      cdTimer += Time.deltaTime;
+      if (cdTimer >= SFXCoolDown)
+      {
+        cdTimer = 0f;
+        cding = false;
+      }
+    }
+  }
 
-	void OnCollisionEnter(Collision col){
-
-		//Debug.LogWarning(col.impulse.magnitude);
-
-		if(!cding){
-			if(col.impulse.magnitude>=VelocityCuttoff){
-				cding = true;
-				AudioManager.QueuePlaySoundRoutine(gameObject,SFX);
-			}
-		}
-	}
+  void OnCollisionEnter(Collision col)
+  {
+    if (!cding)
+    {
+      if (col.impulse.magnitude >= VelocityCuttoff)
+      {
+        cding = true;
+        AudioManager.Instance.PlaySound(gameObject, SFX);
+      }
+    }
+  }
 }
