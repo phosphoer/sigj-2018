08b5a33593b6ebadbff9068bf7b52227191dc617
David Evans <david0evans@gmail.com>
2018-11-04 17:18:32 -0800

    update main menu, glass break

--- sigj-2018-unity/Assets/Game/Scripts/Breakable.cs
+++ sigj-2018-unity/Assets/Game/Scripts/Breakable.cs
@@ -2,32 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breakable : MonoBehaviour {
+public class Breakable : MonoBehaviour
+{
 
-	public GameObject DisableOnBreak;
-	public GameObject EnableOnBreak;
-	public Rigidbody ToDisable;
-	public float BreakingPoint = 2f;
+  public GameObject DisableOnBreak;
+  public GameObject EnableOnBreak;
+  public Rigidbody ToDisable;
+  public float BreakingPoint = 2f;
+  public SoundBank BreakSound;
 
-	bool BROKEN = false;
+  bool BROKEN = false;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+  public void OnCollisionEnter(Collision col)
+  {
+    if (col.impulse.magnitude >= BreakingPoint)
+    {
+      BROKEN = true;
+      if (DisableOnBreak) DisableOnBreak.SetActive(false);
+      if (EnableOnBreak) EnableOnBreak.SetActive(true);
+      if (ToDisable) ToDisable.isKinematic = true;
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	public void OnCollisionEnter(Collision col){
-		if(col.impulse.magnitude >= BreakingPoint){
-			BROKEN = true;
-			if(DisableOnBreak) DisableOnBreak.SetActive(false);
-			if(EnableOnBreak) EnableOnBreak.SetActive(true);
-			if(ToDisable) ToDisable.isKinematic = true;
-		}
-	}
+      if (BreakSound != null)
+        AudioManager.Instance.PlaySound(BreakSound);
+    }
+  }
 }
